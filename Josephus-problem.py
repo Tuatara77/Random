9ffe86999c5b72bdb287202f5772b1642e66606b
Tuatara_77 <44623766+Tuatara77@users.noreply.github.com>
2021-12-21 20:20:49 +0000

    Update Josephus-problem.py
    added all the maths stuff compressed into 1 line just because. this reduces the number of lines needed from 7 to 4

--- Josephus-problem.py
+++ Josephus-problem.py
@@ -13,4 +13,7 @@ number = people-power_of_two
 # (this works out to be 2x the difference between n and 2^x plus 1, where 2^x < n)
 position = str((2*number)+1)
 
+#here's the maths stuff all in 1 line just for the fun of it:
+position_in_1 = str((2*(people-(2**(math.floor(math.log2(people))))))+1)
+
 print(f"The optimal position is {position}")
