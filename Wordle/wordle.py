b862ace32f90361143e494a56a8538c1cb96239d
Tuatara_77 <44623766+Tuatara77@users.noreply.github.com>
2022-01-27 20:58:14 +0000

    Create wordle.py

---
+++ Wordle/wordle.py
@@ -0,0 +1,32 @@
+import random
+
+with open("5-letter-words.txt", "r") as words:
+    wordslist = words.readlines()
+for f in range(len(wordslist)):
+    wordslist[f] = wordslist[f][:5]
+
+word = list(random.choice(wordslist))
+
+guesscount = 1
+wordguess = list(input(f"Guess {guesscount}: "))
+while wordguess != word and guesscount < 5:
+
+    for f in range(len(wordguess)):
+        if wordguess[f] == word[f]:
+            print(f"{wordguess[f]} is in the correct spot")
+        elif wordguess[f] in word:
+            print(f"{wordguess[f]} is right but in the wrong place")
+    
+    guesscount += 1
+    wordguess = list(input(f"Guess {guesscount}: "))
+
+if wordguess == word:
+    wordslist.remove(word)
+    with open("5-letter-words.txt", "w") as words:
+        for f in wordslist:
+            words.write(f"{f}\n")
+else:
+    print("".join(word))
+    with open("5-letter-words", "w") as words:
+        for f in wordslist:
+            words.write(f"{f}\n")
