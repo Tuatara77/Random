1be8e5ccfc09422f0f1c47229a20751337be986a
Tuatara_77 <44623766+Tuatara77@users.noreply.github.com>
2022-03-18 08:18:31 +0000

    Added brightness variable

--- rainbow.py
+++ rainbow.py
@@ -1,42 +1,66 @@
 import pygame
 import random
 
-CHANGERATE = 1
-MAXBRIGHTNESS = 255
+CHANGERATE = 5
+BRIGHTNESS = 255
+assert 0 <= BRIGHTNESS <= 255
+
+STARTCOLOURS = [
+	(BRIGHTNESS, random.randint(0,BRIGHTNESS), 0),
+	(BRIGHTNESS, 0, random.randint(0,BRIGHTNESS)),
+	(random.randint(0,BRIGHTNESS), BRIGHTNESS, 0),
+	(0, BRIGHTNESS, random.randint(0,BRIGHTNESS)),
+	(random.randint(0,BRIGHTNESS), 0, BRIGHTNESS),
+	(0, random.randint(0,BRIGHTNESS), BRIGHTNESS)]
 
 def rainbow(rgb: tuple):
     assert len(rgb) == 3 and rgb[0] == int(rgb[0]) and rgb[1] == int(rgb[1]) and rgb[2] == int(rgb[2]), "Tuple must be 3 integers"
     r, g, b = rgb
     
-    if r < MAXBRIGHTNESS and g == 0 and b == MAXBRIGHTNESS:         r += CHANGERATE
-    elif 0 < r <= MAXBRIGHTNESS and g == MAXBRIGHTNESS and b == 0:  r -= CHANGERATE
-    elif r == MAXBRIGHTNESS and g < MAXBRIGHTNESS and b == 0:       g += CHANGERATE
-    elif r == 0 and 0 < g <= MAXBRIGHTNESS and b == MAXBRIGHTNESS:  g -= CHANGERATE
-    elif r == 0 and g == MAXBRIGHTNESS and b < MAXBRIGHTNESS:       b += CHANGERATE
-    elif r == MAXBRIGHTNESS and g == 0 and 0 < b <= MAXBRIGHTNESS:  b -= CHANGERATE
+    if r < BRIGHTNESS and g == 0 and b == BRIGHTNESS:
+    	r += CHANGERATE
+    elif 0 < r <= BRIGHTNESS and g == BRIGHTNESS and b == 0:
+    	r -= CHANGERATE
+    elif r == BRIGHTNESS and g < BRIGHTNESS and b == 0:
+    	g += CHANGERATE
+    elif r == 0 and 0 < g <= BRIGHTNESS and b == BRIGHTNESS:
+    	g -= CHANGERATE
+    elif r == 0 and g == BRIGHTNESS and b < BRIGHTNESS:
+    	b += CHANGERATE
+    elif r == BRIGHTNESS and g == 0 and 0 < b <= BRIGHTNESS:
+    	b -= CHANGERATE
 
-    if r <= 0:             r = 0
-    if r >= MAXBRIGHTNESS: r = MAXBRIGHTNESS
-    if g <= 0:             g = 0
-    if g >= MAXBRIGHTNESS: g = MAXBRIGHTNESS
-    if b <= 0:             b = 0
-    if b >= MAXBRIGHTNESS: b = MAXBRIGHTNESS
+    if r <= 0:          r = 0
+    if r >= BRIGHTNESS: r = BRIGHTNESS
+    if g <= 0:          g = 0
+    if g >= BRIGHTNESS: g = BRIGHTNESS
+    if b <= 0:          b = 0
+    if b >= BRIGHTNESS: b = BRIGHTNESS
 
     return (r,g,b)
 
 
 def main():
-    FULLSCREEN = False
+    FULLSCREEN = True
     pygame.init()
     info = pygame.display.Info()
 
-    if FULLSCREEN: SCREENWIDTH, SCREENHEIGHT = info.current_w, info.current_h; del info
-    else: SCREENWIDTH, SCREENHEIGHT = 500,500; del info
-    if FULLSCREEN: screen = pygame.display.set_mode([SCREENWIDTH, SCREENHEIGHT], pygame.FULLSCREEN)
-    else: screen = pygame.display.set_mode([SCREENWIDTH, SCREENHEIGHT])
+    if FULLSCREEN: 
+    	SCREENWIDTH = info.current_w
+    	SCREENHEIGHT = info.current_h
+    	del info
+    else:
+        SCREENWIDTH = 500
+  	SCREENHEIGHT = 500
+  	del info
+    
+    if FULLSCREEN: 
+  	screen = pygame.display.set_mode([SCREENWIDTH, SCREENHEIGHT], pygame.FULLSCREEN)
+    else: 
+  	screen = pygame.display.set_mode([SCREENWIDTH, SCREENHEIGHT])
 
     clock = pygame.time.Clock()
-    colour = random.choice([(255,0,0), (0,255,0), (0,0,255)])
+    colour = random.choice(STARTCOLOURS)
 
     done = False
     while not done:
@@ -49,8 +73,8 @@ def main():
                     done = True
         
         colour = rainbow(colour)
-        print(colour)
         screen.fill(colour)
+        print(colour)
         pygame.display.flip()
     pygame.quit()
 
