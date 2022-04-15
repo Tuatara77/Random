b8db7c62428aee558594eba3ebe1fea90a9739f0
Tuatara_77 <44623766+Tuatara77@users.noreply.github.com>
2022-04-15 15:49:21 +0100

    added min and max brightness and made easy to read

--- rainbow (with pygame).py
+++ rainbow (with pygame).py
@@ -15,7 +15,7 @@ STARTCOLOURS = [
 	(               MAXBRIGHTNESS,                                  MINBRIGHTNESS,                 random.randrange(MINBRIGHTNESS,MAXBRIGHTNESS))
     ]
 
-def rainbow(rgb: tuple, minbrightness: int=0, maxbrightness: int=0, changerate: int=3):
+def rainbow(rgb: tuple, minbrightness: int=0, maxbrightness: int=255, changerate: int=3):
     assert len(rgb) == 3 and rgb[0] == int(rgb[0]) and rgb[1] == int(rgb[1]) and rgb[2] == int(rgb[2]), "Tuple must be 3 integers"
     assert rgb[0] == maxbrightness or rgb[1] == maxbrightness or rgb[2] == maxbrightness and \
            rgb[0] == minbrightness or rgb[1] == minbrightness or rgb[2] == minbrightness, "There must be values which are equal to the min and max brightness"
