66bb9fd1b06efb3306cbf96813e1612806e8723b
Tuatara_77 <44623766+Tuatara77@users.noreply.github.com>
2022-04-15 15:48:03 +0100

    added min and max brightness and made easy to read

--- rainbow (with pygame).py
+++ rainbow (with pygame).py
@@ -15,18 +15,18 @@ STARTCOLOURS = [
 	(               MAXBRIGHTNESS,                                  MINBRIGHTNESS,                 random.randrange(MINBRIGHTNESS,MAXBRIGHTNESS))
     ]
 
-def rainbow(rgb: tuple, minbrightness, maxbrightness):
+def rainbow(rgb: tuple, minbrightness: int=0, maxbrightness: int=0, changerate: int=3):
     assert len(rgb) == 3 and rgb[0] == int(rgb[0]) and rgb[1] == int(rgb[1]) and rgb[2] == int(rgb[2]), "Tuple must be 3 integers"
     assert rgb[0] == maxbrightness or rgb[1] == maxbrightness or rgb[2] == maxbrightness and \
            rgb[0] == minbrightness or rgb[1] == minbrightness or rgb[2] == minbrightness, "There must be values which are equal to the min and max brightness"
     r, g, b = rgb
 
-    if                   r <  maxbrightness and                 g == minbrightness and                 b == maxbrightness: r += CHANGERATE
-    elif minbrightness < r <= maxbrightness and                 g == maxbrightness and                 b == minbrightness: r -= CHANGERATE
-    elif                 r == maxbrightness and                 g <  maxbrightness and                 b == minbrightness: g += CHANGERATE
-    elif                 r == minbrightness and minbrightness < g <= maxbrightness and                 b == maxbrightness: g -= CHANGERATE
-    elif                 r == minbrightness and                 g == maxbrightness and                 b <  maxbrightness: b += CHANGERATE
-    elif                 r == maxbrightness and                 g == minbrightness and minbrightness < b <= maxbrightness: b -= CHANGERATE
+    if                   r <  maxbrightness and                 g == minbrightness and                 b == maxbrightness: r += changerate
+    elif minbrightness < r <= maxbrightness and                 g == maxbrightness and                 b == minbrightness: r -= changerate
+    elif                 r == maxbrightness and                 g <  maxbrightness and                 b == minbrightness: g += changerate
+    elif                 r == minbrightness and minbrightness < g <= maxbrightness and                 b == maxbrightness: g -= changerate
+    elif                 r == minbrightness and                 g == maxbrightness and                 b <  maxbrightness: b += changerate
+    elif                 r == maxbrightness and                 g == minbrightness and minbrightness < b <= maxbrightness: b -= changerate
 
     if r <= minbrightness: r = minbrightness
     if r >= maxbrightness: r = maxbrightness
@@ -67,9 +67,9 @@ def main():
                     done = True
                     # done = 10000000
         
-        colour = rainbow(colour, MINBRIGHTNESS, MAXBRIGHTNESS)
+        colour = rainbow(colour, MINBRIGHTNESS, MAXBRIGHTNESS, CHANGERATE)
         screen.fill(colour)
-        print(colour)
+        # print(colour)
         # counter += 1
         # print(counter)
         pygame.display.flip()
@@ -82,7 +82,7 @@ if __name__ == "__main__":
 def a():
     colour = (255,0,0)
     for f in range(1000000):
-        colour = rainbow(colour, MINBRIGHTNESS, MAXBRIGHTNESS)
+        colour = rainbow(colour, MINBRIGHTNESS, MAXBRIGHTNESS, CHANGERATE)
 
 # import cProfile
 # cProfile.run("main()")
